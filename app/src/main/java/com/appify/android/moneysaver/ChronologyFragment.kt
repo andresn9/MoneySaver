c73d474c779b90ddf3e7640a6deb81fc38ea7246
MoneySaver <andresfhjd@gmail.com>
2021-11-18 18:09:39 +0100

    //added firebase support //added login feature through firebase for email, twitter, facebook and google accounts. //added placeholder button on settings to test logging out //app doesn't check if the user is logged in when starting yet, you always start on AuthActivity.

--- app/src/main/java/com/appify/android/moneysaver/ChronologyFragment.kt
+++ app/src/main/java/com/appify/android/moneysaver/ChronologyFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.appify.android.moneysaver.databinding.FragmentChronologyBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,24 +20,39 @@ private const val ARG_PARAM2 = "param2"
  */
 class ChronologyFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding:FragmentChronologyBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        _binding = FragmentChronologyBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+        binding.emailSuccesful.setText("Correcto")
+        binding.passwordSuccesful.setText("Bien")
+
+        return view
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chronology, container, false)
+
+
+
     }
+
+
+
 
     companion object {
         /**
--- app/src/main/java/com/appify/android/moneysaver/SettingsFragment.kt
+++ app/src/main/java/com/appify/android/moneysaver/SettingsFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.content.Intent
+import com.appify.android.moneysaver.databinding.FragmentSettingsBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,26 +21,88 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SettingsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+/*IMPLEMENT LOGOUT BUTTON
+
+logOutButton.setOnClickListener{
+        FirebaseAuth.getInstance().signOut()
+        onBackPressed()}
+ */
+
+
+
 class SettingsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
+        }
+
+
+    }
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        binding.logOutButton.setOnClickListener{
+            Firebase.auth.signOut()
+            showHome()
+
+        }
+        return view
     }
+
+
+
+
+    private fun setup(email:String, provider: String){
+
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    private fun showHome(/*email: String, provider: ProviderType*/) {
+
+
+        val AuthIntent = Intent(activity, AuthActivity::class.java).apply {
+            // putExtra("email", email)
+            // putExtra("provider", provider.name)
+        }
+
+
+
+        startActivity(AuthIntent)
+
+
+    }
+
+
+
+
+
+
+
+
 
     companion object {
         /**
